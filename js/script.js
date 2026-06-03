bcdc7c7aa0d044cdd94313a15bad842d5096c21c
offKosss172 <110238513+offKosss172@users.noreply.github.com>
2026-06-03 14:09:05 +0300

    Update script.js
    off

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyHn15k1CT0Q2h06FJqDmBxZ3NjlPxZZveTKbsXKln83W_FI4F6glDZ5PQSNz8Lp_D0Ag/exec';
+// const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyHn15k1CT0Q2h06FJqDmBxZ3NjlPxZZveTKbsXKln83W_FI4F6glDZ5PQSNz8Lp_D0Ag/exec';
 
-function trackEvent(eventName) {
-  fetch(`${SCRIPT_URL}?event=${eventName}`)
-    .catch(error => console.log(error));
-}
+// function trackEvent(eventName) {
+//   fetch(`${SCRIPT_URL}?event=${eventName}`)
+//     .catch(error => console.log(error));
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  trackEvent('visit');
+//   trackEvent('visit');
 
-  document.querySelectorAll('[data-track]').forEach(link => {
-    link.addEventListener('click', function () {
-      trackEvent(this.dataset.track);
-    });
-  });
+//   document.querySelectorAll('[data-track]').forEach(link => {
+//     link.addEventListener('click', function () {
+//       trackEvent(this.dataset.track);
+//     });
+//   });
 
-});
+// });
 
 
 
